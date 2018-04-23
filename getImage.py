55461d22e8c639da360a9d79dfdec865cbf78727
Graham Jones <grahamjones139@gmail.com>
2018-04-23 23:28:12 +0100

    Initial commit - main working thing is logImages.py that will record one image from the camera every minute for training purposes.

---
+++ getImage.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+
+
+
+import cv2
+#from cv2 import cv
+import urllib
+import httplib2                     # Needed to communicate with camera
+import numpy as np
+import time
+
+
+def getImage():
+    """ Use HTTP GET request to retrieve a jpeg image from the camera.
+    Returns an OpenCV greyscale image, or None if there was an error.
+    """
+    #print "getOpenSeizureDetectorData"
+    h = httplib2.Http(".cache")
+    h.add_credentials('guest', 'guest')
+    requestStr = "http://192.168.0.6/image/jpeg.cgi"
+    #print requestStr
+    try:
+        resp, content = h.request(requestStr,
+                                  "GET")
+        if resp['status'] == '200':
+            buf = np.fromstring(content,dtype=np.uint8)
+            img = cv2.imdecode(buf,0)    
+            return img
+        else:
+            print resp,content,resp
+            return None
+    except:
+        print "getImage Error:",sys.exc_info()[0]
+        return None
+
+
+if __name__ == "__main__":
+    cv2.namedWindow("Frame",1)
+    cv2.namedWindow("Mask",1)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+    fgbg = cv2.createBackgroundSubtractorMOG2()
+
+    while True:
+        frame = getImage()
+        if frame is None:
+            print 'Cam not found'
+            exit(-1)
+        else:
+            fgmask = fgbg.apply(frame)
+            fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+            #print "Displaying image..."
+            cv2.imshow("Frame", frame)
+            cv2.imshow("Mask", fgmask)
+            k= cv2.waitKey(1)
+        if k==0x1b:
+            print 'Esc. Exiting'
+            break
+        time.sleep(1)
---
+++ logImages.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import cv2
+from getImage import getImage
+#from cv2 import cv
+import urllib
+import httplib2                     # Needed to communicate with camera
+import numpy as np
+import time
+import datetime
+import os
+
+outDir = "./images"
+
+
+if (os.path.exists(outDir)):
+    if (os.path.isdir(outDir)):
+        print "Output Directory already exists - OK"
+    else:
+        print "ERROR:  %s exists, but is not a Directory" % outDir
+        exit(-1)
+else:
+    print "Creating output directory %s" % outDir
+    os.makedirs(outDir)
+
+
+
+while True:
+    frame = getImage()
+    if frame is None:
+        print 'Error Retrieving Image'
+        exit(-1)
+    else:
+        tVal = datetime.datetime.now()
+        fname = "img_%s.png" % (tVal.strftime("%Y%d%m%H%M%S"))
+        print "fname=%s" % (fname)
+        cv2.imwrite(os.path.join(outDir,fname),frame)
+        
+    time.sleep(60)
